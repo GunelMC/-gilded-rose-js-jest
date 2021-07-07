e25b83d8fcb197e192c0925b76d6d4880fa39cc5
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-07 14:33:04 +0100

    First test with jest passing

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -4,6 +4,6 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
 });
