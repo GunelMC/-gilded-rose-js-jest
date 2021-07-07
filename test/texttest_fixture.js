8e4c005aeff80602873b9e0415ac52e6acaa1314
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-07 23:08:15 +0100

    Change TestTest_fixture to observe sample test data beahavior

--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,4 +1,5 @@
-const { Shop, Item } = require('../src/gilded_rose');
+const Shop = require('../src/shop');
+const Item = require('../src/item');
 
 const items = [
   new Item('+5 Dexterity Vest', 10, 20),
@@ -14,7 +15,7 @@ const items = [
   new Item('Conjured Mana Cake', 3, 6),
 ];
 
-const days = Number(process.argv[2]) || 2;
+const days = Number(process.argv[2]) || 5;
 const gildedRose = new Shop(items);
 
 console.log('OMGHAI!');
