fd50d920f0043c2ded198e741381c319047bc6ed
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-08 09:10:49 +0100

    Add shop unit test: updateQuality decreaases SellIn day property for reg items

--- test/shop.test.js
+++ test/shop.test.js
@@ -2,16 +2,23 @@ const Shop = require('../src/shop');
 const Item = require('../src/item');
 
 describe('Shop', () => {
-  it('should foo', () => {
+  it('creates an item called "foo"', () => {
     const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
 
   it('should decrease quality by 1', () => {
-    const cheese = new Item('Cheese', 2, 2);
-    const gildedRose = new Shop([cheese]);
+    const cheeseDouble = { sellIn: 30, quality: 10 };
+    const gildedRose = new Shop([cheeseDouble]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(1);
+    expect(items[0].quality).toEqual(9);
+  });
+
+  it('should decrease sell in by 1', () => {
+    const cheeseDouble = { sellIn: 30, quality: 10 };
+    const gildedRose = new Shop([cheeseDouble]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(29);
   });
 });
