53caa19a4883b68a09e528d321b368f75b54453d
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-08 16:26:43 +0100

    Move max quality of 50 to Aged Brie test block

--- test/shop.test.js
+++ test/shop.test.js
@@ -25,15 +25,16 @@ describe('Shop', () => {
     });
   });
 
-  describe('quality range', ()=> {
-
+  describe('minimum quality', () => {
     it('quality can\'t go below 0', () => {
       cheeseDouble = { name: 'cheese', sellIn: 0, quality: 0 };
       gildedRose = new Shop([cheeseDouble]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(0);
     });
+  });
 
+  describe('Aged Brie', ()=> {
     it('quality can\'t go above 50 for regular items', () => {
       cheeseDouble = { name: 'Aged Brie', quality: 49 };
       gildedRose = new Shop([cheeseDouble]);
