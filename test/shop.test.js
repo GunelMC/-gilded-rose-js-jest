a8f77f77333d1ed32886f96ce5e4403c514efb7b
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-08 16:08:06 +0100

    Add a test case for max quality for regular items

--- test/shop.test.js
+++ test/shop.test.js
@@ -26,14 +26,20 @@ describe('Shop', () => {
   });
 
   describe('quality range', ()=> {
-    beforeEach(() => {
-      cheeseDouble = { name: 'cheese', sellIn: 0, quality: 0 };
-      gildedRose = new Shop([cheeseDouble]);
-    });
 
     it('quality can\'t go below 0', () => {
+      cheeseDouble = { name: 'cheese', sellIn: 0, quality: 0 };
+      gildedRose = new Shop([cheeseDouble]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(0);
+    });
+
+    it('quality can\'t go above 50 for regular items', () => {
+      cheeseDouble = { name: 'Aged Brie', quality: 49 };
+      gildedRose = new Shop([cheeseDouble]);
+      items = gildedRose.updateQuality();
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
     });
   });
 });
