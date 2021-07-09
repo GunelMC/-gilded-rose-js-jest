e863df3f88cb9f5773b209c3f561c186a4651846
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-07-09 12:01:33 +0100

    Replace items.forEach with gildedRose.items in golden master test

--- test/golden_master.test.js
+++ test/golden_master.test.js
@@ -7,19 +7,19 @@ describe('Golden master integration test', () => {
     let actualOutput = '';
     const expectedMasterOutput = fs.readFileSync(`${__dirname}/golden_master.txt`, { encoding: 'utf8' });
 
-  const items = [
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    const items = [
+      new Item('+5 Dexterity Vest', 10, 20),
+      new Item('Aged Brie', 2, 0),
+      new Item('Elixir of the Mongoose', 5, 7),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
 
-    // This Conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
-  ]; 
+      // This Conjured item does not work properly yet
+      new Item('Conjured Mana Cake', 3, 6)
+    ];
 
     const gildedRose = new Shop(items);
     const days = 30;
@@ -28,7 +28,8 @@ describe('Golden master integration test', () => {
       actualOutput += `\n-------- day ${i} --------\n`;
       actualOutput += 'name, sellIn, quality\n';
 
-      items.forEach((item) => {
+      // eslint-disable-next-line no-loop-func
+      gildedRose.items.forEach((item) => {
         actualOutput += `${item.name}, ${item.sellIn}, ${item.quality}\n`;
       });
 
